6f90eba5ca8198636718f6baf42fc7d5c12d3692
Kod-e <115715642+Kod-e@users.noreply.github.com>
2025-08-17 18:09:33 +0900

    add new feedback view

--- src/views/RecordFeedBackView.vue
+++ src/views/RecordFeedBackView.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-    <h1>RecordFeedBackView</h1>
+    <h1 class="text-lg text-gray-900 mb-4">{{ t('suggestion') }}</h1>
     <div v-if="controller.suggestion">
-      <p>{{ controller.suggestion }}</p>
+      <p class="text-md text-gray-900 mb-2">{{ controller.suggestion }}</p>
     </div>
-    <div v-for="judge_result in controller.judge_results" :key="judge_result.question">
-      <p>{{ judge_result.question }}</p>
-      <p>{{ judge_result.answer }}</p>
-      <p>{{ judge_result.correct_answer }}</p>
-      <p>{{ judge_result.error_reason }}</p>
+
+    <div
+      :class="'bg-' + (judge_result.correct ? 'green' : 'red') + '-100 p-6 rounded-md'"
+      v-for="judge_result in controller.judge_results"
+      :key="judge_result.question"
+    >
+      <p class="text-md text-gray-900 mb-2">{{ t('question') }}</p>
+      <p class="text-md text-gray-900 mb-2">{{ judge_result.question }}</p>
+      <p class="text-md text-gray-900 mb-2" v-if="judge_result.correct">{{ t('correct') }}</p>
+      <p class="text-md text-gray-900 mb-2" v-else>{{ t('incorrect') }}</p>
+      <p class="text-md text-gray-900 mb-2">{{ t('answer') }}</p>
+      <p class="text-md text-gray-500 mb-2">{{ judge_result.answer }}</p>
+      <div v-if="!judge_result.correct">
+        <p class="text-md text-gray-900 mb-2">{{ t('correct_answer') }}</p>
+        <p class="text-md text-gray-500 mb-2">{{ judge_result.correct_answer }}</p>
+        <p class="text-md text-gray-900 mb-2">{{ t('error_reason') }}</p>
+        <p class="text-md text-gray-500 mb-2">{{ judge_result.error_reason }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { questionController } from '@/controller/question'
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
 
 const controller = questionController()
 </script>
